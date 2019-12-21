372387b1ea5a7068b7c7ab499a6da43c6368c84a
Felix Batusic <felix@batusic.net>
2019-12-21 22:06:29 +0100

    got it working again

--- src/main/java/com/nullwert/annilyser/main/Main.java
+++ src/main/java/com/nullwert/annilyser/main/Main.java
@@ -1,7 +1,5 @@
 package com.nullwert.annilyser.main;
 
-import com.sun.javafx.application.LauncherImpl;
-
 public class Main {
     //public static void main(String[] args) {
     //    LauncherImpl.launchApplication(AnnilyserApplication.class, AnnilyserPreloader.class, args);
