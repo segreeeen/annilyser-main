24ced19f9e7cbe65bf100625e771887f19b6e045
Felix Batusic <felix@batusic.net>
2019-12-29 21:35:02 +0100

    got it working again

--- src/main/java/com/nullwert/annilyser/main/model/GameState.java
+++ src/main/java/com/nullwert/annilyser/main/model/GameState.java
@@ -1,6 +1,7 @@
 package com.nullwert.annilyser.main.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nullwert.annilyser.parser.token.Token;
 import lombok.Data;
 
 import java.util.HashMap;
@@ -9,37 +10,46 @@ import java.util.Optional;
 
 @Data
 public class GameState {
-    Map<String, IStatisticComponent> gameState;
-    Map<Long, Map<String, Map<String, Long>>> nexusradar;
+
+    private GameStateData gameState;
 
     public GameState(IStatisticComponent killsDeath) {
-        this.gameState = new HashMap<>();
-        this.nexusradar = new HashMap<>();
-        gameState.put("killsDeath", killsDeath);
-    }
-
-    public void setKillsDeath(IStatisticComponent killsDeath) {
-        gameState.put("killsDeath", killsDeath);
+        this.gameState = new GameStateData(killsDeath);
     }
 
     public static GameState getDefaultGameState() {
         return new GameState(KillsDeath.createDefaultKillsDeath());
     }
 
-    public void addNexusRadarValue(Long second, String nexusteam, String victimteam) {
-        Map<String, Map<String, Long>> nexusteamMap = nexusradar.computeIfAbsent(second, k -> new HashMap<>());
-        Map<String, Long> victimteamMap = nexusteamMap.computeIfAbsent(nexusteam, k -> new HashMap<>());
-        if (victimteamMap.get(victimteam) != null) {
-            victimteamMap.put(victimteam, victimteamMap.get(victimteam) + 1);
-        } else {
-            victimteamMap.put(victimteam, 1L);
+    @Data
+    public class GameStateData {
+        IStatisticComponent killsDeath;
+        Map<Long, Map<String, Map<String, Long>>> nexusradar;
+
+        public GameStateData(IStatisticComponent killsDeath) {
+            this.killsDeath = killsDeath;
+            this.nexusradar = new HashMap<>();
         }
-    }
 
-    @JsonIgnore
-    public Long getLatestNexusRadarEntry() {
-        Optional<Long> o = nexusradar.keySet().stream().max(Long::compare);
-        return o.orElse(0L);
+        public void setKillsDeath(IStatisticComponent killsDeath) {
+            this.killsDeath = killsDeath;
+        }
 
+        public void addNexusRadarValue(Long second, String nexusteam, String victimteam) {
+            Map<String, Map<String, Long>> nexusteamMap = nexusradar.computeIfAbsent(second, k -> new HashMap<>());
+            Map<String, Long> victimteamMap = nexusteamMap.computeIfAbsent(nexusteam, k -> new HashMap<>());
+            if (victimteamMap.get(victimteam) != null) {
+                victimteamMap.put(victimteam, victimteamMap.get(victimteam) + 1);
+            } else {
+                victimteamMap.put(victimteam, 1L);
+            }
+        }
+
+        @JsonIgnore
+        public Long getLatestNexusRadarEntry() {
+            Optional<Long> o = nexusradar.keySet().stream().max(Long::compare);
+            return o.orElse(0L);
+
+        }
     }
 }
